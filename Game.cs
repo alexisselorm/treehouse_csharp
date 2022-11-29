08b7313817e9127032a4ddaa4a0b0e80e438c5ec
Alexis Selorm Gbeckor-Kove <54552023+alexisselorm@users.noreply.github.com>
2022-11-29 06:42:28 +0000

    Scopes, Fields and compiling.

--- Game.cs
+++ Game.cs
@@ -1,10 +1,15 @@
-namespace TreehouseDefense{
+using System;
+using TreehouseDefense;
+
   class Game
   {
-    public void Main(){
-      System.Console.WriteLine("Hello");
+    public static void Main(){
+      Console.WriteLine("Hello");
     
     Tower tower = new Tower();
+      Map map = new Map();
+      map.Width = 8;
+      map.Height= 5;
+      int area  = map.Width * map.Height;
     }
   }
-}
--- Map.cs
+++ Map.cs
@@ -1,5 +1,7 @@
 namespace TreehouseDefense{
   class Map{
-    
+    // If scope is not specifiend, it is private by default;
+    public int Width;
+    public int Height;
   }
 }
