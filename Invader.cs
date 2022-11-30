05c5b08f95ce127f0947360f4e3ca8dd7526f316
Alexis Selorm Gbeckor-Kove <54552023+alexisselorm@users.noreply.github.com>
2022-11-30 21:34:44 +0000

    Using properties.

--- Invader.cs
+++ Invader.cs
@@ -1,9 +1,23 @@
 namespace TreehouseDefense{
   class Invader{
 
+    private int _pathStep = 0;
+    private readonly Path _path;
     // Accessor Methods. Getters and setters
     // Syntactic sugar for getters and setters
     // Other class will get the invader's location on the map, but they can't set it. This will allow only the invader itself to set its new location
     public MapLocation Location{ get; private set; }
+
+    // Since the setter is private we can only set the location of an invader in its class
+    public Invader (Path path) {
+      _path = path
+      Location = _path.GetLocationAt(_pathStep);
+    }
+    public void Move()
+    {
+      _pathStep+=1;
+      Location = _path.GetLocationAt(_pathStep);
+      
+    }
   }
 }
