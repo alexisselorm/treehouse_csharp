89513873c906119d1e299e7ade27e23a643a0fef
Alexis Selorm Gbeckor-Kove <54552023+alexisselorm@users.noreply.github.com>
2022-11-30 17:15:23 +0000

    Handling and Throwing Exceptions

--- Game.cs
+++ Game.cs
@@ -4,23 +4,17 @@ using TreehouseDefense;
   class Game
   {
     public static void Main(){
-      Console.WriteLine("Hello");
 
       Map map = new Map(8,5);
 
-      MapLocation x = new MapLocation(4,2);
-      bool isOnMap = map.OnMap(x);
-      Console.WriteLine(isOnMap);
-      x = new MapLocation(8,5);
-      isOnMap = map.OnMap(x);
-      Console.WriteLine(isOnMap);
-      Console.WriteLine(x.DistanceTo(5,5));
-
-      // Type checks
-      Console.WriteLine(x is MapLocation);
-      Console.WriteLine(x is Point);
-      Point point = new Point(3,3);
-      Console.WriteLine(point is MapLocation);
+      try{
+        
+      MapLocation maplocation = new MapLocation(40,20,map);
+      }
+      catch (Exception){
+        Console.WriteLine("That map location is not on map");
+      }
+     
       
     }
   }
--- MapLocation.cs
+++ MapLocation.cs
@@ -4,9 +4,14 @@ namespace TreehouseDefense
   {
 // Constructor. Name your constructor the same as your class.
 // base is the parent class, here it is Point
-    public MapLocation(int x, int y) : base(x, y)
+    public MapLocation(int x, int y, Map map) : base(x, y)
     {
-     
+// Using the map's onmap method to determine if the map location being constructed is on the map. 
+// This refers to the object that is being constructed. 'In this scope'
+     if(!map.OnMap(this))
+     {
+       throw new System.Exception();  
+     }
     }
     
   }
