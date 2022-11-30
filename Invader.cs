3b6bd2feacdb2c58d4516dc103dfb3a043b7eb6d
Alexis Selorm Gbeckor-Kove <54552023+alexisselorm@users.noreply.github.com>
2022-11-30 21:43:57 +0000

    Computed properties

--- Invader.cs
+++ Invader.cs
@@ -6,17 +6,21 @@ namespace TreehouseDefense{
     // Accessor Methods. Getters and setters
     // Syntactic sugar for getters and setters
     // Other class will get the invader's location on the map, but they can't set it. This will allow only the invader itself to set its new location
-    public MapLocation Location{ get; private set; }
+    public MapLocation Location
+    {
+      get{
+        // Get the invader's current location. This is a computed property.
+      return _path.GetLocationAt(_pathStep);
+    } 
+     private set; }
 
     // Since the setter is private we can only set the location of an invader in its class
     public Invader (Path path) {
       _path = path
-      Location = _path.GetLocationAt(_pathStep);
     }
     public void Move()
     {
       _pathStep+=1;
-      Location = _path.GetLocationAt(_pathStep);
       
     }
   }
