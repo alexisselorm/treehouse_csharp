41c1936051f4630a63d9e9e075f46530c5e46778
Alexis Selorm Gbeckor-Kove <54552023+alexisselorm@users.noreply.github.com>
2022-11-30 21:47:37 +0000

    Syntactic sugar for bodied members

--- Invader.cs
+++ Invader.cs
@@ -6,22 +6,14 @@ namespace TreehouseDefense{
     // Accessor Methods. Getters and setters
     // Syntactic sugar for getters and setters
     // Other class will get the invader's location on the map, but they can't set it. This will allow only the invader itself to set its new location
-    public MapLocation Location
-    {
-      get{
         // Get the invader's current location. This is a computed property.
-      return _path.GetLocationAt(_pathStep);
-    } 
-     private set; }
+    public MapLocation Location => _path.GetLocationAt(_pathStep);
+      
 
     // Since the setter is private we can only set the location of an invader in its class
     public Invader (Path path) {
       _path = path
     }
-    public void Move()
-    {
-      _pathStep+=1;
-      
-    }
+    public void Move() => _pathStep+=1;
   }
 }
